d849c5fd06a2be163bc672301557ed3c75d81dc2
Hugo Piso <hugopiso@yahoo.com>
2016-04-09 01:09:12 +0200

    ajout des fixtures places

---
+++ www/src/AppBundle/DataFixtures/ORM/LoadPlaceData.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Place;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadPlaceData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $place = new Place();
+        $place->setName('Tour Eiffel');
+        $manager->persist($place);
+
+        $place2 = new Place();
+        $place2->setName('Musée du louvre');
+        $manager->persist($place2);
+
+        $place3 = new Place();
+        $place3->setName('Sacré Coeur');
+        $manager->persist($place3);
+
+        $manager->flush();
+    }
+}
