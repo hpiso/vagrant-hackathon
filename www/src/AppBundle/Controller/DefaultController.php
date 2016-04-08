6e11952615641c9d9bdb3c61971599afd686ee94
Hugo Piso <hugopiso@yahoo.com>
2016-04-09 01:20:31 +0200

    ajout du form sur la hp

--- www/src/AppBundle/Controller/DefaultController.php
+++ www/src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\PlaceType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -12,6 +13,11 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('AppBundle:Default:index.html.twig');
+
+        $form = $this->createForm(PlaceType::class, null);
+
+        return $this->render('AppBundle:Default:index.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ www/src/AppBundle/Form/PlaceType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaceType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('place', EntityType::class, [
+                'class' => 'AppBundle:Place',
+                'choice_label' => 'name',
+                'attr' => [
+                    'class' => 'form-control input-lg select2'
+                ]
+            ])
+        ;
+    }
+
+}
