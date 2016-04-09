1d42ce4150743e0b23d7389542a3df76dd450a3d
Hugo Piso <hugopiso@yahoo.com>
2016-04-09 16:43:06 +0200

    mise en place de gmap

--- www/src/AppBundle/Controller/DefaultController.php
+++ www/src/AppBundle/Controller/DefaultController.php
@@ -5,19 +5,30 @@ namespace AppBundle\Controller;
 use AppBundle\Form\PlaceType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="homepage")
+     * @Method({"GET", "POST"})
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-
         $form = $this->createForm(PlaceType::class, null);
+        $form->handleRequest($request);
+        $place = null;
+
+        if ($form->isSubmitted()) {
+            $place = $form->getData()['place'];
+
+            dump($place);die;
+        }
 
         return $this->render('AppBundle:Default:index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'place' => $place
         ]);
     }
 }
